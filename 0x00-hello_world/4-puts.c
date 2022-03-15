ed5bf84d3fa42d488cd6621e5b7692e25f487dc6
Miliyonayalew <mila@DESKTOP-1ULGF16.localdomain>
2022-03-14 23:52:00 -0700

    pushed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - entry point
+*
+*Return: after printing the function
+*/
+int main(void)
+{
+puts("\"Programming is like building a multinlingual puzzle");
+return (0);
+}
