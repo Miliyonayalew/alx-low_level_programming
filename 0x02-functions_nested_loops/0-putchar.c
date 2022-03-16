cbc7681c90d34347a38b0d35fcfda578ba546c36
Miliyonayalew <mila@DESKTOP-1ULGF16.localdomain>
2022-03-16 08:15:53 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,19 @@
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-putchar.c -o 0-putchar
+#include <main.h>
+
+/**
+ * main -entry block
+ * @void: no argument
+ * Return: 0
+ **/
+int main(void)
+{
+        char ermias[] = "_putchar";
+
+	int c;
+	for(c=0 c < 8; c++)
+	{
+		_putchar(ermias[c]);
+	}
+	_putchar('\n')
+	return (0);
+}
