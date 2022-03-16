e6cfe80f43a045fdaad47cf94a2fc1732c1d82db
Miliyonayalew <miliayalew@gmail.com>
2022-03-16 12:29:12 -0700

    my commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,14 +12,13 @@
 
 int main(void)
 {
-	int sum, num;
-	
-	for (num = 0; num < 1024; ++num)
-	{
-		if ((num % 3 == 0) || (num % 5 == 0))
-			sum += num;
-	}
-
-	printf("%d\n", sum);
-	return (0);
+		int sum, num;
+			
+			for (num = 0; num < 1024; ++num)
+					{
+								if ((num % 3 == 0) || (num % 5 == 0))
+												sum += num;
+									}
+				printf("%d\n", sum);
+					return (0);
 }
