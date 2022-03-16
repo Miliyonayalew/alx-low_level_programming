9654d085c162f3cd2bee56d98c7c169425266807
Miliyonayalew <miliayalew@gmail.com>
2022-03-16 10:21:08 -0700

    my commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,54 +1,41 @@
-#include "main.h"
+#include"main.h"
 
 /**
- *  * print_times_table - prints the times table
- *   * @n: integer for which the times table will be printed
- *    *
- *     * Description: prints the times table
- *      *
- *       * Return: void
- *        */
+ *  * print_times_table - prints time table of n
+ *   *
+ *    * @n: takes number input
+ *    */
 
 void print_times_table(int n)
 {
-		int row, column, product;
+		int prod, mult, num;
 
-			if (n >= 0 && n < 15)
+			if (n <= 15 && n >= 0)
 					{
-								for (row = 0; row <= n; row++)
+								for (num = 0; num <= n; ++num)
 											{
-															for (column = 0; column <= n; column++)
-																			{
-																								product = row * column;
+															_putchar(48);
+																		for (mult = 1; mult <= n; ++mult)
+																						{
+																											_putchar(',');
+																															_putchar(' ');
 
-																												if (column == 0)
-																																		_putchar('0');
-																																else if (product < 10)
-																																					{
-																																											_putchar(',');
-																																																_putchar(' ');
-																																																					_putchar(' ');
-																																																										_putchar(' ');
-																																																															_putchar(product % 10 + '0');
-																																																																			}
-																																				else if (product >= 10 && product < 100)
-																																									{
-																																															_putchar(',');
-																																																				_putchar(' ');
-																																																									_putchar(' ');
-																																																														_putchar((product / 10) % 10 + '0');
-																																																																			_putchar(product % 10 + '0');
-																																																																							}
-																																								else if (product > 99 && product < 1000)
-																																													{
-																																																			_putchar(',');
-																																																								_putchar(' ');
-																																																													_putchar(product / 100 + '0');
-																																																																		_putchar((product / 10) % 10 + '0');
-																																																																							_putchar(product % 10 + '0');
-																																																																											}
-																																											}
-																		_putchar('\n');
-																				}
+																																			prod = num * mult;
+
+																																							if (prod <= 9)
+																																													_putchar(' ');
+																																											if (prod <= 99)
+																																																	_putchar(' ');
+
+																																															if (prod >= 100)
+																																																				{
+																																																										_putchar((prod / 100) + 48);
+																																																															_putchar((prod / 10) % 10 + 48);
+																																																																			} else if (prod <= 99 && prod >= 10)
+																																																																									_putchar((prod / 10) + 48);
+																																																																							_putchar((prod % 10) + 48);
+																																																																										}
+																					_putchar('\n');
+																							}
 									}
 }
