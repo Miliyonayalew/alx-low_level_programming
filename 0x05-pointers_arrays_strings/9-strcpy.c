7d8640686b24837c8b1328d8cc84f7acc8bf6357
Miliyon Ayalew <35680940+Miliyonayalew@users.noreply.github.com>
2022-03-22 00:04:01 -0700

    5th commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
- * _strcpy - copies the string pointed to by src
- * @dest: A pointer to a char that will be changed
- * @src: A pointer to a char that will be changed
- * Return: dest
- **/
+ *_strcpy - copies the string pointed to by src
+ *@dest: A pointer to a char that will be changed
+ *@src: A pointer to a char that will be changed
+ *Return: dest
+ */
 
 char *_strcpy(char *dest, char *src)
 {
 
-	int i;
+int i;
 
-	i = 0;
+i = 0;
 
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
 
-	return (dest);
+return (dest);
 
 }
